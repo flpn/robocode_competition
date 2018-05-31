55c59ab6fd254b4c828f9d28c3f8e19879a1757b
Felipe Netto <flpncc@gmail.com>
2018-05-30 22:04:51 -0300

    Creating views and templates

---
+++ hotsite/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'hotsite'
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('usuario/<int:pk>', views.SuccessRegisterView.as_view(), name='success-register'),    
+    path('registrar/', views.CreatePlayerView.as_view(), name='create-player'),
+    path('partidas/', views.ListMatchesView.as_view(), name='list-matches')
+]
--- hotsite/views.py
+++ hotsite/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView, DetailView, ListView
 
-# Create your views here.
+from .models import Player, Match
+from .forms import PlayerForm
+
+
+class HomeView(TemplateView):
+    template_name = 'hotsite/home.html'
+
+
+class SuccessRegisterView(DetailView):
+    model = Player
+
+
+class CreatePlayerView(CreateView):
+    form_class = PlayerForm
+    template_name = 'hotsite/create_player.html'
+
+
+class ListMatchesView(ListView):
+    def get_queryset(self):
+        return Match.objects.all()
